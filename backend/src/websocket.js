5b76cf3ead3dafc565cd238947315abc979c9c06
IsraelJMameli <israel.mmeli@gmail.com>
2020-01-20 22:44:07 -0300

    Utilização de WebSocket para RealTime

---
+++ backend/src/websocket.js
@@ -0,0 +1,37 @@
+const socketio = require('socket.io');
+const parseStringAsArray = require ('./utils/parseStringAsArray'); 
+const calculatedDistance = require ('./utils/calculateDistance');
+let io;
+const connections = [];
+
+exports.setupWebsocket = (server) => {
+     io = socketio(server);
+
+    io.on('connection', socket  => {
+       
+    const {latitude, longitude, techs } = socket.handshake.query;
+
+        connections.push({
+            id: socket.id,
+            coordinates: {
+                latitude: Number(latitude),
+                longitude: Number(longitude),
+            },
+            techs: parseStringAsArray(techs),
+        });
+    });
+};
+
+exports.findConnections = (coordiantes, techs) => {
+    return connections.filter(connection => {
+        return calculatedDistance(coordiantes, connection.coordinates) < 10
+            && connection.techs.some(item => techs.includes(item))
+    })
+}
+
+exports.sendMessage = (to, message, data) => {
+    to.forEach(connection => {
+        io.to(connection.id).emit(message, data);
+
+    })
+}
